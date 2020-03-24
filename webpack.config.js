433b281fb87fc6fd662e995fea7e02984ab249ce
xoanyiwo <onyemobi.anyiwo@jmsmucker.com>
2020-03-24 15:08:40 -0400

    Add Countdown form component and tests

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,8 @@ module.exports = {
       Navigation: 'app/components/Navigation.jsx',
       Timer: 'app/components/Timer.jsx',
       Clock: 'app/components/Clock.jsx',
-      Countdown: 'app/components/Countdown.jsx',     
+      Countdown: 'app/components/Countdown.jsx',   
+      CountdownForm: 'app/components/CountdownForm.jsx',  
       applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
